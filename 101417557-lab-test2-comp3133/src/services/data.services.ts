964ab6a875bb24725aa2811b8bc12344f1eeb019
Andrej03 <96391874+Andrej03@users.noreply.github.com>
2025-04-02 12:29:23 -0400

    mission list, missionfilter and service for data set not tested

---
+++ 101417557-lab-test2-comp3133/src/services/data.services.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private apiUrl = 'https://api.spacexdata.com/v4/launches';
+
+  constructor(private http: HttpClient) { }
+
+  getLaunches(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+}
